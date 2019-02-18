c2fc5a8b426b35e5a4219a531ba0bd0b742adf71
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-18 19:01:15 +0700

    Styling React With CSS Classes

--- src/components/MyApp.js
+++ src/components/MyApp.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import Footer from './Footer'
+import Content from './MyContent'
+import Header from './Header'
 
 function MyApp() {
     return (
         <div>
-            <input type="checkbox" />
+            {/* <input type="checkbox" />
             <p>Place Holder Text Here</p>
             <input type="checkbox" />
             <p>Place Holder Text Here</p>
             <input type="checkbox" />
-            <p>Place Holder Text Here</p>
+            <p>Place Holder Text Here</p> */}
+            <Header />
+            <Content />
+            <Footer />
         </div>
     )
 }
