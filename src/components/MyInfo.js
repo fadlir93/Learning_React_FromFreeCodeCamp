0c50dc97be2d26c6b8cdc234182e1bea2e5d803f
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-18 08:49:02 +0700

    Move components into seperate Files

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MyApp(){
+    return (
+        <div>
+            <h1>Fadli Ramadhan</h1>
+            <p>My name is Fadli Ramadhan , i'm 25 Year's Old, My Last Education Formal is Vocational High School Media Informatika</p>
+            <ul>
+                <li>Beach</li>
+                <li>Mountain</li>
+                <li>Hill</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyApp
--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import MyInfo from './components/MyInfo'
 
-function Introduce(){
-    return (
-        <div>
-            <h1>Fadli Ramadhan</h1>
-            <p>My name is Fadli Ramadhan , i'm 25 Year's Old, My Last Education Formal is Vocational High School Media Informatika</p>
-            <ul>
-                <li>Beach</li>
-                <li>Mountain</li>
-                <li>Hill</li>
-            </ul>
-        </div>
-    )
-}
 
-ReactDOM.render(<Introduce />, document.getElementById('root'))
+ReactDOM.render(<MyInfo />, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
