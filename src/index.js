5d612805cdb6f37ec21bfb296d41973e6e412b62
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-18 19:21:12 +0700

    Jsx to javascript

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MyApp from './components/MyApp'
+// import MyApp from './components/MyApp'
 
-ReactDOM.render(<MyApp />, document.getElementById('root'))
+function App() {
+    let firstName = "Fadli"
+    let lastName = "Ramadhan"
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    if(hours < 12) {
+        timeOfDay = "Morning"
+    } else if(hours > 12 && hours < 17) {
+        timeOfDay = "Evening"
+    } else {
+        timeOfDay = "Night"
+    }
+    return (
+        <div>
+            <h1>My Name is {`${firstName} ${lastName}`}</h1>
+            <h3>It's Currently {date.getHours() % 12} O'clock</h3>
+            <h2>Good {timeOfDay}</h2>
+        </div>
+        
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
