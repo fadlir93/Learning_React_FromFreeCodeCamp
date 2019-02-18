bcd070cabe93e45f5c2982e2f03ecacc131f3df1
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-18 08:37:37 +0700

    make a package react-dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Introduce(){
+    return (
+        <div>
+            <h1>Fadli Ramadhan</h1>
+            <p>My name is Fadli Ramadhan , i'm 25 Year's Old, My Last Education Formal is Vocational High School Media Informatika</p>
+            <ul>
+                <li>Beach</li>
+                <li>Mountain</li>
+                <li>Hill</li>
+            </ul>
+        </div>
+    )
+}
 
+ReactDOM.render(<Introduce />, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
