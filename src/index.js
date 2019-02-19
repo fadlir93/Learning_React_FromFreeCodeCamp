a59a63a2474a5c390ff0ad8cb9c55bae395dedd5
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-19 07:21:27 +0700

    Inline Style With Style Property

--- src/index.js
+++ src/index.js
@@ -9,18 +9,27 @@ function App() {
     const hours = date.getHours()
     let timeOfDay
 
+    const page3 = {
+        fontSize: '30',
+        backgroundColor: 'grey',
+    }
+
     if(hours < 12) {
         timeOfDay = "Morning"
+        page3.color = 'red'
     } else if(hours > 12 && hours < 17) {
         timeOfDay = "Evening"
+        page3.color = 'pink'
     } else {
         timeOfDay = "Night"
+        page3.color = 'green'
     }
+    
     return (
         <div>
-            <h1>My Name is {`${firstName} ${lastName}`}</h1>
+            <h1 style={{color: 'yellow', backgroundColor:'brown'}}>My Name is {`${firstName} ${lastName}`}</h1>
             <h3>It's Currently {date.getHours() % 12} O'clock</h3>
-            <h2>Good {timeOfDay}</h2>
+            <h2 style={page3}>Good {timeOfDay}</h2>
         </div>
         
     )
